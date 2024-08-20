9b780db894800d52b7ceca99b4784505c3058309
yongqulee <169157861+yongqulee@users.noreply.github.com>
2024-08-20 12:42:02 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,9 +4,9 @@ setup(
     name='rltrader',
     version='3.2.1',
     description='Quantylab Reinforcement Learning for Stock Trading',
-    author='Quantylab',
-    author_email='quantylab@gmail.com',
-    url='https://github.com/quantylab/rltrader',
+    author='YQLEE',
+    # author_email='quantylab@gmail.com',
+    # url='https://github.com/quantylab/rltrader',
     packages=find_namespace_packages(where='src', include=['quantylab.*']),
     package_dir={'': 'src'},
     install_requires=[
@@ -15,8 +15,8 @@ setup(
         'matplotlib',
         'mplfinance',
         'tqdm',
-        'sklearn',
+        'scikit-learn',
         'tensorflow==2.7.0',
-        'torch==1.10.1',
+        'torch==2.2.0',
     ]
 )
